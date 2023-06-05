c91396b9a66d57d656b90310b89375fa19e5c7e1
pycoder74 <105924451+pycoder74@users.noreply.github.com>
2023-06-05 08:54:28 +0100

    Update getweather.py

--- src/getweather.py
+++ src/getweather.py
@@ -1,24 +1,29 @@
 import aiohttp
 import tkinter as tk
 import asyncio
+import numpy as np
+import datetime as dt
+from datetime import datetime
+import urllib.request
+import sys
+import json
+
 async def get_weather(master=None):
-    url=f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bristol"
-    params = {
-        "unitGroup": "metric",
-        "include": "current",
-        "key": "5H2F2M6K2X4E9LXKH7UP3DNMQ",
-        "contentType": "json"
-    }
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, params=params) as response:
-            if response.status != 200:
-                print('Unexpected status code:', response.status)
-            jsonData = await response.json()
-            current_temp = jsonData['currentConditions']['temp']
-            current_temp = f"{current_temp}°C"
-            print(current_temp)
-            weather=tk.Label(master, text=current_temp, font=('calibri', 20))
-            weather.pack(anchor='center', side=tk.LEFT, padx=100)
+    try: 
+      ResultBytes = urllib.request.urlopen("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Bristol?unitGroup=metric&elements=temp&include=current%2Cfcst&key=3UQS226XB878JKZZPG2JDJWDQ&contentType=json")
+      
+      # Parse the results as JSON
+      jsonData = json.load(ResultBytes)
+      print(jsonData['currentConditions'])
+    except urllib.error.HTTPError  as e:
+      ErrorInfo= e.read().decode() 
+      print('Error code: ', e.code, ErrorInfo)
+      sys.exit()
+    except  urllib.error.URLError as e:
+      ErrorInfo= e.read().decode() 
+      print('Error code: ', e.code,ErrorInfo)
+      sys.exit()
 if __name__ == '__main__':
     win=tk.Tk()
     asyncio.run(get_weather(win))
+
